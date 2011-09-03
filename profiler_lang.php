cf62ef606cc1517d0da3e30cebb3fe7f25b708c3
Thomas Traub <thomastraub2000@yahoo.de>
2011-09-03 15:33:44 +0200

    Updated to v2.0.3

--- profiler_lang.php
+++ profiler_lang.php
@@ -9,6 +9,7 @@ $lang['profiler_post_data']		= 'DONNEES POST';
 $lang['profiler_uri_string']	= 'URI STRING';
 $lang['profiler_memory_usage']	= 'UTILISATION MEMOIRE';
 $lang['profiler_config']		= 'VARIABLES CONFIG';
+$lang['profiler_session_data']	= 'DONNEES SESSION';
 $lang['profiler_headers']		= 'HTTP HEADERS';
 $lang['profiler_no_db']			= "Le driver de la base de donn&eacute;es n'est actuellement pas charg&eacute;";
 $lang['profiler_no_queries']	= "Aucune requ&ecirc;te n'a &eacute;t&eacute; ex&eacute;cut&eacute;e";
@@ -17,6 +18,8 @@ $lang['profiler_no_get']		= "Aucune donn&eacute;e GET n'existe";
 $lang['profiler_no_uri']		= "Aucune donn&eacute;e URI n'existe";
 $lang['profiler_no_memory']		= 'Utilisation de la m&eacute;moire indisponible';
 $lang['profiler_no_profiles']	= 'Pas de donn&eacute;es du profil - toute section est desactiv&eacute;e.';
+$lang['profiler_section_hide']	= 'Cacher';
+$lang['profiler_section_show']	= 'Montrer';
 
 /* End of file profiler_lang.php */
 /* Location: ./system/language/french/profiler_lang.php */
